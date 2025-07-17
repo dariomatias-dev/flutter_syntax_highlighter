2e89a45230c10315914e51eda101d9a09c997e36
dariomatias-dev <matiasdario75@gmail.com>
2025-07-17 06:02:05 -0300

    doc(lib/src/syntax_token.dart): added documentation
    - Developed class documentation for improved clarity.

--- lib/src/syntax_token.dart
+++ lib/src/syntax_token.dart
@@ -1,9 +1,17 @@
 import 'package:flutter_syntax_highlighter/src/token_type.dart';
 
+/// Represents a token with its type, textual value, and optional nesting level.
+///
+/// Used during syntax highlighting to classify parts of source code.
 class SyntaxToken {
   SyntaxToken(this.type, this.value, {this.level = 0});
 
+  /// The type of this token.
   final TokenType type;
+
+  /// The level of nesting or scope of the token.
   final String value;
+
+  /// The level of nesting or scope of the token.
   final int level;
 }
