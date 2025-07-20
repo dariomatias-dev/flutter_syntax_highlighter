1c8efa147de04ab5ebfeaa9d2f6bc127cda8476c
dariomatias-dev <matiasdario75@gmail.com>
2025-07-20 15:23:06 -0300

    feat(lib/src/syntax_highlighter.dart): performance improvements
    - Applied `ListView.builder` for performance enhancements.

--- lib/src/syntax_highlighter.dart
+++ lib/src/syntax_highlighter.dart
@@ -5,7 +5,7 @@ import 'package:flutter_syntax_highlighter/src/syntax_theme.dart';
 import 'package:flutter_syntax_highlighter/src/token_type.dart';
 
 /// Displays Dart/Flutter source code with syntax highlighting and optional line numbers and selection.
-class SyntaxHighlighter extends StatelessWidget {
+class SyntaxHighlighter extends StatefulWidget {
   const SyntaxHighlighter({
     super.key,
     required this.code,
@@ -38,109 +38,148 @@ class SyntaxHighlighter extends StatelessWidget {
   /// Maximum number of characters for line numbering.
   final int? maxCharCount;
 
-  double _calculateLineWidth({
-    required double fontSize,
-    required double lineHeight,
-    required int maxLineNumberDigits,
-  }) {
-    const characterWidthFactor = 0.6;
-    const extraSpacing = 8.0;
-
-    final characterWidth = fontSize * characterWidthFactor;
-    final numberWidth = characterWidth * maxLineNumberDigits;
-
-    return numberWidth + extraSpacing;
-  }
-
   @override
-  Widget build(BuildContext context) {
-    final baseTheme = isDarkMode ? SyntaxTheme.dark() : SyntaxTheme.light();
-    final theme = baseTheme.copyWithFontSize(fontSize);
-    final highlighter = Highlighter(theme);
+  State<SyntaxHighlighter> createState() => _SyntaxHighlighterState();
+}
 
-    final tokens = highlighter.tokenize(code);
+class _SyntaxHighlighterState extends State<SyntaxHighlighter> {
+  late List<TextSpan> _lines;
+  late Highlighter _highlighter;
+  late SyntaxTheme _theme;
+  late int _maxDigits;
+  late double _lineNumberWidth;
 
-    final lineWidgets = <Widget>[];
-    var currentLineSpans = <TextSpan>[];
-    var lineNumber = 1;
+  void _setupAndProcess() {
+    final baseTheme = widget.isDarkMode
+        ? SyntaxTheme.dark()
+        : SyntaxTheme.light();
+    _theme = baseTheme.copyWithFontSize(widget.fontSize);
+    _highlighter = Highlighter(_theme);
 
-    final maxDigits =
-        maxCharCount ?? ('\n'.allMatches(code).length + 1).toString().length;
+    _maxDigits =
+        widget.maxCharCount ??
+        ('\n'.allMatches(widget.code).length + 1).toString().length;
 
-    final lineWidth = _calculateLineWidth(
-      fontSize: fontSize,
-      lineHeight: lineHeight,
-      maxLineNumberDigits: maxDigits,
+    _lineNumberWidth = _calculateLineNumberWidth(
+      fontSize: widget.fontSize,
+      maxLineNumberDigits: _maxDigits,
     );
 
-    void addLine() {
-      lineWidgets.add(
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            if (showLineNumbers)
-              SelectionContainer.disabled(
-                child: Container(
-                  width: lineWidth,
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(
-                    lineNumber.toString(),
-                    textAlign: TextAlign.right,
-                    style: theme.lineNumberStyle.copyWith(height: lineHeight),
-                  ),
-                ),
-              ),
-            Expanded(
-              child: Text.rich(
-                TextSpan(
-                  style: theme.baseStyle.copyWith(height: lineHeight),
-                  children: List.of(currentLineSpans),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
+    _lines = _processCode();
+  }
 
-      currentLineSpans.clear();
-      lineNumber++;
-    }
+  /// Pre-processes the code into a list of styled [TextSpan]s, one for each line.
+  List<TextSpan> _processCode() {
+    final tokens = _highlighter.tokenize(widget.code);
+    final processedLines = <TextSpan>[];
+    var currentLineSpans = <TextSpan>[];
 
     for (final token in tokens) {
       if (token.type == TokenType.newline) {
-        addLine();
+        processedLines.add(TextSpan(children: List.of(currentLineSpans)));
+
+        currentLineSpans.clear();
       } else if (token.value.contains('\n')) {
-        final lines = token.value.split('\n');
-        for (var i = 0; i < lines.length; i++) {
-          final line = lines[i];
+        final linesInToken = token.value.split('\n');
+        for (var i = 0; i < linesInToken.length; i++) {
+          final line = linesInToken[i];
           if (line.isNotEmpty) {
             currentLineSpans.add(
-              TextSpan(text: line, style: highlighter.getStyleForToken(token)),
+              TextSpan(text: line, style: _highlighter.getStyleForToken(token)),
             );
           }
-          if (i < lines.length - 1) {
-            addLine();
+          if (i < linesInToken.length - 1) {
+            processedLines.add(TextSpan(children: List.of(currentLineSpans)));
+            currentLineSpans.clear();
           }
         }
       } else {
         currentLineSpans.add(
           TextSpan(
             text: token.value,
-            style: highlighter.getStyleForToken(token),
+            style: _highlighter.getStyleForToken(token),
           ),
         );
       }
     }
 
     if (currentLineSpans.isNotEmpty) {
-      addLine();
+      processedLines.add(TextSpan(children: List.of(currentLineSpans)));
     }
 
-    final codeBlock = Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: lineWidgets,
+    return processedLines;
+  }
+
+  double _calculateLineNumberWidth({
+    required double fontSize,
+    required int maxLineNumberDigits,
+  }) {
+    const characterWidthFactor = 0.6;
+    const padding = 16.0;
+
+    final characterWidth = fontSize * characterWidthFactor;
+    final numberWidth = characterWidth * maxLineNumberDigits;
+
+    return numberWidth + padding;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setupAndProcess();
+  }
+
+  @override
+  void didUpdateWidget(SyntaxHighlighter oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.code != widget.code ||
+        oldWidget.isDarkMode != widget.isDarkMode ||
+        oldWidget.fontSize != widget.fontSize) {
+      _setupAndProcess();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final codeBlock = ListView.builder(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      itemCount: _lines.length,
+      itemBuilder: (context, index) {
+        final lineNumber = index + 1;
+
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            if (widget.showLineNumbers)
+              SelectionContainer.disabled(
+                child: Container(
+                  width: _lineNumberWidth,
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text(
+                    lineNumber.toString(),
+                    textAlign: TextAlign.right,
+                    style: _theme.lineNumberStyle.copyWith(
+                      height: widget.lineHeight,
+                    ),
+                  ),
+                ),
+              ),
+            Expanded(
+              child: Text.rich(
+                _lines[index],
+                style: _theme.baseStyle.copyWith(height: widget.lineHeight),
+              ),
+            ),
+          ],
+        );
+      },
     );
 
-    return enableCodeSelection ? SelectionArea(child: codeBlock) : codeBlock;
+    return widget.enableCodeSelection
+        ? SelectionArea(child: codeBlock)
+        : codeBlock;
   }
 }
