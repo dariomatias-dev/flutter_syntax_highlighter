f9acfb533e5d41f00568d2215978543cd84ad4d3
dariomatias-dev <matiasdario75@gmail.com>
2025-08-24 09:06:04 -0300

    fix(lib/src/themes/vs_code_dark_theme.dart): code formatting correction
    - Fixed code formatting issues.

--- lib/src/themes/vs_code_dark_theme.dart
+++ lib/src/themes/vs_code_dark_theme.dart
@@ -3,22 +3,22 @@ import 'package:flutter_syntax_highlighter/src/syntax_color_schema.dart';
 
 class VSCodeDarkTheme extends SyntaxColorSchema {
   const VSCodeDarkTheme()
-      : super(
-          baseStyle: const Color(0xFFD4D4D4),
-          lineNumberStyle: const Color.fromARGB(179, 255, 255, 255),
-          keywordStyle: const Color(0xFF569CD6),
-          specialKeywordStyle: const Color(0xFF569CD6),
-          storageModifierStyle: const Color(0xFF569CD6),
-          typeStyle: const Color(0xFF4EC9B0),
-          functionStyle: const Color(0xFFD4D4D4),
-          literalStyle: const Color(0xFFD4D4D4),
-          commentStyle: const Color(0xFF6A9955),
-          punctuationStyle: const Color(0xFFFFFFFF),
-          stringStyle: const Color(0xFFD69D85),
-          numberStyle: const Color(0xFFB5CEA8),
-          bracket1Style: const Color(0xFFD4D4D4),
-          bracket2Style: const Color(0xFF569CD6),
-          bracket3Style: const Color(0xFF4EC9B0),
-          variableStyle: const Color(0xFF9CDCFE),
-        );
+    : super(
+        baseStyle: const Color(0xFFD4D4D4),
+        lineNumberStyle: const Color.fromARGB(179, 255, 255, 255),
+        keywordStyle: const Color(0xFF569CD6),
+        specialKeywordStyle: const Color(0xFF569CD6),
+        storageModifierStyle: const Color(0xFF569CD6),
+        typeStyle: const Color(0xFF4EC9B0),
+        functionStyle: const Color(0xFFD4D4D4),
+        literalStyle: const Color(0xFFD4D4D4),
+        commentStyle: const Color(0xFF6A9955),
+        punctuationStyle: const Color(0xFFFFFFFF),
+        stringStyle: const Color(0xFFD69D85),
+        numberStyle: const Color(0xFFB5CEA8),
+        bracket1Style: const Color(0xFFD4D4D4),
+        bracket2Style: const Color(0xFF569CD6),
+        bracket3Style: const Color(0xFF4EC9B0),
+        variableStyle: const Color(0xFF9CDCFE),
+      );
 }
